66f96a0e5c9faa32235b7f2f7517a30f6b71c440
matthew-burkhard <matthew.burkhard@gmail.com>
2023-07-06 16:21:42 -0400

    move things

--- lambda.tf
+++ lambda.tf
@@ -1,3 +1,22 @@
+
+resource "aws_api_gateway_rest_api" "my_api" {
+  name        = "my-api"
+  description = "My API"
+}
+
+resource "aws_api_gateway_resource" "my_resource" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
+  path_part   = "my-resource"
+}
+
+resource "aws_api_gateway_method" "my_method" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.my_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
 resource "aws_api_gateway_integration" "my_integration" {
   rest_api_id             = aws_api_gateway_rest_api.my_api.id
   resource_id             = aws_api_gateway_resource.my_resource.id
