86f5a03296a892edbe28beeee3db751ac3e72baf
matthew-burkhard <matthew.burkhard@gmail.com>
2023-07-10 16:19:18 -0400

    this

--- lambda.tf
+++ lambda.tf
@@ -50,6 +50,15 @@ resource "aws_lambda_function" "my_lambda_function" {
   }
 }
 
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.my_lambda_function.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+}
+
 # resource "aws_iam_role" "lambda_execution_role" {
 #   name = "lambda-execution-role"
 
